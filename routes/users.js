ea59b4f16e97880b9bcecc8eb2e35c9c2a89798a
aryan216 <aniketani603@gmail.com>
2024-01-10 23:47:51 +0530

    backend auth

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const mongoose=require("mongoose");
+const plm=require("passport-local-mongoose")
+mongoose.connect("mongodb://127.0.0.1:27017/pintrest");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const userSchema=mongoose.Schema({
+  username:String,
+  name:String,
+  email:String,
+  password:String,
+  profileImage:String,
+  contact:Number,
+  posts:{
+    type:Array,
+    default:[]
+  }
 });
-
-module.exports = router;
+userSchema.plugin(plm)
+module.exports=mongoose.model("user",userSchema);
